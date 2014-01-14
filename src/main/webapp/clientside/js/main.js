68205455529dfbe905e6c44920657cb357dce14d
sovanegger <martin.tomsovsky@seznam.cz>
2014-01-14 01:27:40 +0100

    #11: deleting unused

--- src/main/webapp/clientside/js/main.js
+++ src/main/webapp/clientside/js/main.js
@@ -1,7 +1,5 @@
 var APP_NAME = "/chat-spring-angular";
 var LOGIN_PAGE_URL = "/login";
-var CHAT_MESSAGE_LIMIT = 10;
-var chatAppControllers = angular.module('chatAppControllers', []);
 
 var hasStorage = (function() {
 	if(typeof(Storage)!=='undefined')
